08ab77426ee14f0fde6a099d7b4adb68c05479a0
Kelium Jojoa Urrutia <keliumju@gmail.com>
2024-07-13 19:21:07 -0500

    Remove web-components tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
-import lit from "@astrojs/lit";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://kelium-blog.netlify.app/",
-  integrations: [lit(), preact()]
+  integrations: [preact()]
 });
--- src/components/lit-web-components/sl-button.js
+++
@@ -1,3 +0,0 @@
-import '@shoelace-style/shoelace/dist/components/button/button.js';
-
-export const tagName = 'sl-button';
